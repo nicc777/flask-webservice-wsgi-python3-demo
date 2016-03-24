ad813b18d032895aadc4a932e68d6377777bb5a6
root <nicc777@gmail.com>
2016-03-24 06:07:40 +0200

    Started working on unittest

--- fwsdemo/database.py
+++ fwsdemo/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-dbString = os.environ.get('DB_CONN_STR') or 'sqlite:///test2.db'
+dbString = os.environ.get('DB_CONN_STR') or 'sqlite:///unittest.db'
 print("Using dbString=%s" % dbString)
 
 engine = create_engine(dbString, convert_unicode=True)
@@ -17,4 +17,3 @@ def init_db():
     # you will have to import them first before calling init_db()
     import fwsdemo.models
     Base.metadata.create_all(bind=engine)
-
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,7 +1,59 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
+import unittest
+from fwsdemo.database import init_db
+from fwsdemo.database import db_session
+from fwsdemo.models import User
+
+def prep_data():
+    try:
+        u1 = User('person1', 'p1@example.tld')
+        u2 = User('person2', 'p2@example.tld')
+        db_session.bulk_save_objects([u1,u2])
+        db_session.commit()
+        users = User.query.all()
+        print(users)
+    except:
+        db_session.rollback()
+        print("WARNING: Couldn't create new test data.")
+
+def delete_all_data():
+    try:
+        num_rows_deleted = db_session.query(Model).delete()
+        db_session.commit()
+    except:
+        db_session.rollback()
+
+def setUpModule():
+    print("setUpModule")
+    init_db()
+
+def tearDownModule():
+    print("tearDownModule")
 
 
-def test_success():
-    assert True
+class Class1Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("  setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("  tearDownClass")
+
+    def setUp(self):
+        print("       setUp")
+        prep_data()
+
+    def tearDown(self):
+        print("       tearDown")
+        delete_all_data()
+
+    def test_1(self):
+        print("  class 1 test 1")
+        record = db_session.query(User).filter(User.name == 'person1').first()
+        assertEqual(record.email, 'p1@example.tld', 'Matching the correct email to the correct person')
+
+    def test_2(self):
+        print("  class 1 test 2")
+        record = db_session.query(User).filter(User.name == 'person2').first()
+        assertEqual(record.email, 'p2@example.tld', 'Matching the correct email to the correct person')
